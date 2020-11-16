5b9d78696d0d6880e08310901dedfa417e6b9c98
Brandon Elzy <brandon.g.elzy@gmail.com>
2020-11-16 08:40:45 -0600

    Set up testing suite

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
 
